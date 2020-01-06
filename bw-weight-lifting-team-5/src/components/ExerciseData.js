c480e35022d62e2d2555ee703963b927a6150533
David Tauraso <david318@me.com>
2020-01-06 14:27:26 -0800

    Setup mock interface for CRUD operations

--- bw-weight-lifting-team-5/src/components/ExerciseData.js
+++ bw-weight-lifting-team-5/src/components/ExerciseData.js
@@ -3,29 +3,32 @@
 // workouts -> exercises
 // exercise
 
-let workouts = {
+export let workouts = {
     1: [0, 1, 2]
 }
 
-let exercises = {
+export let exercises = {
 
     1: [
             {
                 exerciseName: 'test 1',
                 reps: 20,
-                bodyPart: 'arm'
+                bodyPart: 'arm',
+                id: 0
             },
 
             {
                 exerciseName: 'test 2',
                 reps: 10,
-                bodyPart: 'leg'
+                bodyPart: 'leg',
+                id: 1
             },
 
             {
                 exerciseName: 'test 3',
                 reps: 35,
-                bodyPart: 'hand'
+                bodyPart: 'hand',
+                id: 2
             }
         ]
 
