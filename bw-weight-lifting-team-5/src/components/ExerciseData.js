f1496787490aa7d3f34762a611d26568494f3d61
David Tauraso <david318@me.com>
2020-01-06 10:46:53 -0800

    Added mock server code and package.json for mock login for the token passing part of mvp

---
+++ bw-weight-lifting-team-5/src/components/ExerciseData.js
@@ -0,0 +1,37 @@
+
+// token -> workouts
+// workouts -> exercises
+// exercise
+
+let workouts = {
+    1: [0, 1, 2]
+}
+
+let exercises = {
+
+    1: [
+            {
+                exerciseName: 'test 1',
+                reps: 20,
+                bodyPart: 'arm'
+            },
+
+            {
+                exerciseName: 'test 2',
+                reps: 10,
+                bodyPart: 'leg'
+            },
+
+            {
+                exerciseName: 'test 3',
+                reps: 35,
+                bodyPart: 'hand'
+            }
+        ]
+
+}
+
+// token -> workouts
+let users = {
+    'really-long-token-string' : [1]
+}
